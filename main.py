c772197daadba5c8adcc3889459c0bb426ee670d
Aleksandr Pyatkin <av.pyatkin@yandex.ru>
2022-06-02 23:41:25 +0400

    End Project

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+    shuffle(password_list)
+
+    gen_password = "".join(password_list)
+    entry_password.delete(0, END)
+    entry_password.insert(0, gen_password)
+    pyperclip.copy(gen_password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def clear_field():
@@ -23,7 +43,7 @@ def save():
         clear_field()
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                               f"\nPassword: {password} \nIs it OK to save?")
+                                                              f"\nPassword: {password} \nIs it OK to save?")
         if is_ok:
             with open("data.txt", mode="a") as datafile:
                 datafile.write(password_entry)
@@ -62,7 +82,7 @@ entry_password = Entry(width=21)
 entry_password.grid(column=1, row=3)
 
 # Buttons
-button_generate = Button(text="Generate Password")
+button_generate = Button(text="Generate Password", command=generate_password)
 button_generate.grid(column=2, row=3)
 
 button_add = Button(text="Add", width=36, command=save)
